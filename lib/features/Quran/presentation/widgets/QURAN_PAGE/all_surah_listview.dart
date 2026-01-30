d91cc89b671e28daa99eb4c7befcf1200ad3245c
muha06 <mhassan040206@gmail.com>
2026-01-30 10:57:24 +0300

    fix

--- lib/features/Quran/presentation/widgets/QURAN_PAGE/all_surah_listview.dart
+++ lib/features/Quran/presentation/widgets/QURAN_PAGE/all_surah_listview.dart
@@ -6,7 +6,6 @@ import 'package:rafeeq/core/themes/light_colors.dart';
 import 'package:rafeeq/features/Quran/domain/entities/surah.dart';
 import 'package:rafeeq/features/Quran/presentation/pages/surah_page.dart';
 import 'package:rafeeq/features/Quran/presentation/riverpod/fetch_surahs_provider.dart';
-import 'package:rafeeq/features/Quran/presentation/widgets/SURAH_PAGE/surah_pageview_wrapper.dart';
 import 'package:rafeeq/features/settings/presentation/provider/theme_provider.dart';
 import 'package:shimmer/shimmer.dart';
 
