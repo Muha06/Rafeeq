de556bcc7a9e016a78c8e616aa95c0f74fd6bb79
muha06 <mhassan040206@gmail.com>
2026-02-16 11:28:14 +0300

    fix

---
+++ lib/features/quran_goal/domain/entities/quran_goal.dart
@@ -0,0 +1,6 @@
+class QuranGoal {
+  final int dailyTarget; // e.g., 50 ayahs
+  final DateTime createdAt; // optional, track when goal was set
+
+  QuranGoal({required this.dailyTarget, required this.createdAt});
+}
---
+++ lib/features/quran_goal/domain/entities/quran_log.dart
@@ -0,0 +1,6 @@
+class QuranLog {
+  final DateTime date; // normalize to yyyy-mm-dd
+  final int ayahsRead;
+
+  QuranLog({required this.date, required this.ayahsRead});
+}
